d65829a3111098241db68cc1e7aee99940812bf2
PeterMonev <monevpeter@gmail.com>
2023-05-29 16:14:08 +0300

    Create home.js

---
+++ JS Applications Exam - 3 December 2022/Music Library/src/views/home.js
@@ -0,0 +1,18 @@
+import { html,render } from "../lib.js";
+import { getUserData } from "../util.js";
+
+const homeTemplate = () => html` <section id="home">
+<img src="./images/landing.png" alt="home" />
+
+<h2 id="landing-text"><span>Add your favourite albums</span> <strong>||</strong> <span>Discover new ones right
+    here!</span></h2>
+</section>>
+`;
+
+export function homeView(ctx){
+    if(getUserData()){
+        ctx.page.redirect('/catalog')
+    } else {
+        ctx.render(homeTemplate());
+    }
+}
